0bb938485a0498891edf5b5de1dfbfc584148119
Scott Fraser <scottafraser@yahoo.com>
2018-09-12 13:19:18 -0700

    yeti timeout works

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,49 @@ import Pet from "./Pet";
 import Menu from "./Menu";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.
+    this.state = {
+      status: "Alive",
+      feed: 100,
+      play: 100,
+      sleep: 100
+    };
+    this._time();
   }
+
+  _time() {
+    this.runAway();
+    this.setState({ feed: this.state.feed - 10 });
+    this.setState({ play: this.state.play - 10 });
+    this.setState({ sleep: this.state.sleep - 10 });
+    setTimeout(this._time.bind(this), 500);
+  }
+
+  runAway() {
+    if (
+      this.state.feed === 0 ||
+      this.state.feed === 0 ||
+      this.state.feed === 0
+    ) {
+      this.setState({ status: (this.state.status = "DEAD") });
+    }
+  }
+
+  cancelTime() {
+    if (this.state.status == "DEAD") {
+      clearTimeout(this._time());
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Pet />
+        <p>{this.state.status}</p>
+        <p>{this.state.feed}</p>
+        <p>{this.state.feed}</p>
+        <p>{this.state.feed}</p>
         <Menu />
       </div>
     );
