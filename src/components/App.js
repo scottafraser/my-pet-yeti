713ec995d3e3b1ce9ccea83e9403d49bdc4ffcda
Student <example@example.com>
2018-09-13 11:09:00 -0700

    yeti functions work

--- src/components/App.js
+++ src/components/App.js
@@ -18,15 +18,18 @@ class App extends Component {
       slept: false
     };
     this.handleFeed = this.handleFeed.bind(this);
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handleSleep = this.handleSleep.bind(this);
+
   }
 
   componentDidMount() {
-    this.petTimer = setInterval(() => this.updatePetTime(), 1000);
+    this.petTimer = setInterval(() => this.updatePetTime(), 4000);
   }
 
   updatePetTime() {
     this.handleStatus();
-    this.setState({ feed: this.state.feed - 10 });
+    this.setState({ feed: this.state.feed - 20 });
     this.setState({ play: this.state.play - 10 });
     this.setState({ sleep: this.state.sleep - 10 });
   }
@@ -48,32 +51,57 @@ class App extends Component {
  
   handleFeed() {
       this.setState({ feed: this.state.feed + 10 });
-      this.setState({ fed: true })  
+      this.setState({ fed: true })   
+      setTimeout(
+        function () {
+          this.setState({ fed: false });
+        }
+          .bind(this),
+        1000
+      );  
   }
 
   handlePlay() {
-    this.setState({ feed: this.state.play + 10 });
-    this.setState({ fed: true })
+    this.setState({ play: this.state.play + 10 });
+    this.setState({ played: true })
+    setTimeout(
+      function () {
+        this.setState({ fed: false });
+      }
+        .bind(this),
+      1000
+    );  
   }
 
   handleSleep() {
-    this.setState({ feed: this.state.sleep + 10 });
-    this.setState({ fed: true })
+    this.setState({ sleep: this.state.sleep + 10 });
+    this.setState({ slept: true })
+    setTimeout(
+      function () {
+        this.setState({ fed: false });
+      }
+        .bind(this),
+      1000
+    );  
   }
 
   render() {
-    return (
-      <div className="App">
-        <Pet fed={this.state.fed}/>
+    return <div className="App">
+        <Pet fed={this.state.fed} played={this.state.played} slept={this.state.slept} />
         <p>{this.state.status}</p>
         <p>{this.state.feed}</p>
         <p>{this.state.play}</p>
         <p>{this.state.sleep}</p>
-        <PrimaryButton onClick={this.handleFeed} icon={<Icon src="add" />}>Feed</PrimaryButton>
-        <PrimaryButton onClick={this.handlePlay} icon={<Icon src="add" />}>Play</PrimaryButton>
-        <PrimaryButton onClick={this.handleSleep} icon={<Icon src="add" />}>Sleep</PrimaryButton>
-      </div>
-    );
+        <PrimaryButton onClick={this.handleFeed} icon={<Icon src="add" />}>
+          Feed
+        </PrimaryButton>
+        <PrimaryButton onClick={this.handlePlay} icon={<Icon src="add" />}>
+          Play
+        </PrimaryButton>
+        <PrimaryButton onClick={this.handleSleep} icon={<Icon src="add" />}>
+          Sleep
+        </PrimaryButton>
+      </div>;
   }
 }
 
